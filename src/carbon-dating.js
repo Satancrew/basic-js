846be0e3e7e6daccd83a7f189640120e1738e7e8
Satancrew <saintwinter@yandex.ru>
2022-05-02 15:25:54 +0300

    feat: carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+
 
 /**
  * Determine the age of archeological find by using
@@ -17,9 +16,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+
+function dateSample(sampleActivity) {
+  
+  let shortName = sampleActivity;
+  if (typeof shortName !== 'string' || !shortName || isNaN(shortName) || shortName <= 0 || shortName > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  +shortName;
+  let test = 0.693 / HALF_LIFE_PERIOD;
+  let answer = Math.ceil(Math.log(MODERN_ACTIVITY / shortName) / test);
+  return answer;
 }
 
 module.exports = {
